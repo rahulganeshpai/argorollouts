14c164cc874c51ec98d436cdcb058497cb5ec525
rahulganeshpai <rahul.ganesh.pai@sap.com>
2023-11-22 10:32:34 +0100

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
+# Install minikube and addons
 minikube start --memory=8192mb --cpus=4
 minikube addons enable metrics-server
 minikube addons enable istio-provisioner
 minikube addons enable istio
-curl -L https://istio.io/downloadIstio | sh -
-cd istio-1.20.0
+# Install istio and addons
+ISTIO_VERSION=1.20.0
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${ISTIO_VERSION} TARGET_ARCH=x86_64 sh -
+cd istio-${ISTIO_VERSION}
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
-kubectl label namespace dev istio-injection=enabled
-kubectl create namespace argo-rollouts
-kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.20/samples/addons/prometheus.yaml
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.20/samples/addons/kiali.yaml
+# Create dev namespace
+cd ..
+kubectl apply -f namespace.yaml
+# Install argo-rollouts
+kubectl create namespace argo-rollouts
+kubectl label namespace argo-rollouts istio-injection=enabled
+kubectl apply -n argo-rollouts -f https://github.com/argoproj/argo-rollouts/releases/latest/download/install.yaml
